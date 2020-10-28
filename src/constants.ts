e27c59197232816250e1f7a6d0938a23c3155839
pablomag <pablomag@gmail.com>
2020-10-28 12:11:23 +0300

    Removed unused constants

--- src/constants.ts
+++ src/constants.ts
@@ -3,18 +3,12 @@ export const PROJECT_VERSION = "1.1.0";
 
 export const ENV_PRODUCTION = process.env.NODE_ENV === "production";
 
-export const API_URI = process.env.API_URI || "http://localhost";
 export const API_PORT = process.env.API_PORT || 5555;
+export const API_URL = process.env.API_URL ?? `http://localhost:${API_PORT}`;
 export const API_SECRET = process.env.API_SECRET || "bubblegum_cat";
-export const API_URL = API_URI === "http://localhost" ? `${API_URI}:${API_PORT}` : API_URI;
 
-export const CLIENT_URI = process.env.CLIENT_URI || "http://localhost";
-export const CLIENT_PORT = process.env.CLIENT_PORT || 3000;
-export const CLIENT_URL = CLIENT_URI === "http://localhost" ? `${CLIENT_URI}:${CLIENT_PORT}` : CLIENT_URI;
-
-export const IMG_SERVICE_URI = process.env.IMG_SERVICE_URI || "http://localhost";
-export const IMG_SERVICE_PORT = process.env.IMG_SERVICE_PORT || 3333;
-export const IMG_SERVICE_URL = IMG_SERVICE_URI === "http://localhost" ? `${IMG_SERVICE_URI}:${IMG_SERVICE_PORT}` : IMG_SERVICE_URI;
+export const CLIENT_URL = process.env.CLIENT_URL ?? "http://localhost:3000";
+export const IMG_SERVICE_URL = process.env.IMG_SERVICE_URL ?? "http://localhost:3333";
 
 export const REDIS_HOST = process.env.REDIS_HOST || "http://localhost";
 export const REDIS_PORT = parseInt(process.env.REDIS_PORT!) || 6379;
